05f14b5e0184077fe7b88ff846d35d55ed9901c4
이주성 <32183520@dankook.ac.kr>
2024-07-10 23:18:39 +0900

    Critical Section

---
+++ src/main/java/org/example/ch05/ex03/CriticalSectionExample.java
@@ -0,0 +1,40 @@
+package org.example.ch05.ex03;
+
+public class CriticalSectionExample {
+    public static void main(String[] args) {
+        SharedResource resource = new SharedResource();
+
+        Thread t1 = new Thread(resource::increment);
+        Thread t2 = new Thread(resource::increment);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class SharedResource {
+    private int counter = 0;
+
+    public void increment() {
+        for (int i = 0; i < 100000; i++) {
+
+            synchronized (this) {   // Entry Section
+                counter++;
+                System.out.println(Thread.currentThread().getName() + ": " + counter);
+            }   // Exit Section
+        }
+
+        // Remainder Section
+        doOtherWork();
+    }
+
+    private void doOtherWork() {
+        System.out.println(Thread.currentThread().getName()
+                + " 는 critical section 외부에서 작업을 수행 중..");
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+}
---
+++ src/main/java/org/example/ch05/ex03/RaceConditionExample.java
@@ -0,0 +1,34 @@
+package org.example.ch05.ex03;
+
+// 실행할 때마다 값이 달라지는 Race Condition
+
+public class RaceConditionExample {
+    private static int sharedResource = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 스레드 100개를 생성해 공유 리소스를 동시에 증가시킨다
+        Thread[] incrementThreads = new Thread[100];
+        for (int i = 0; i < incrementThreads.length; i++) {
+            incrementThreads[i] = new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    sharedResource++; // 공유 데이터 동시 접속
+                }
+            });
+            incrementThreads[i].start();
+        }
+
+        // 모든 스레드가 작업을 완료할 때까지 대기
+        for (Thread t : incrementThreads) {
+            t.join();
+        }
+
+        System.out.println("Expected Value: " + (100 * 10000));
+        System.out.println("Actual value: " + sharedResource);
+    }
+}
+
+/*
+    Expected Value: 1000000
+    Actual value: 979406
+ */
